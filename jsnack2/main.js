b2b65d60666eb538ffd694f31d808355e4baedf6
Marco Sandri <sandrimarco98@gmail.com>
2022-01-27 17:21:18 +0100

    jsnack 2

---
+++ jsnack2/main.js
@@ -0,0 +1,8 @@
+let primin = prompt('inserisci un numero');
+let printCube = document.getElementById("print");
+
+for(let i = 0; i < primin; i++) {
+    
+    let cube = Math.pow(i+1, 3)
+    printCube.innerHTML += ((i+1) + " al cubo è " + cube + "<hr>");
+}
